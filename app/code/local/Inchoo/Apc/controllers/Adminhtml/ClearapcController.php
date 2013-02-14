8bce4dc9871a1be12c18ab55fd46bd7b55fb93cb
Zvonimir Buric <zvonimir.buric@inchoo.net>
2013-02-14 14:55:57 +0100

    First commit

---
+++ app/code/local/Inchoo/Apc/controllers/Adminhtml/ClearapcController.php
@@ -0,0 +1,29 @@
+<?php
+
+    class Inchoo_Apc_Adminhtml_ClearapcController extends Mage_Adminhtml_Controller_Action
+    {
+        public function indexAction()
+        {
+            if(function_exists('apc_clear_cache '))
+            {
+                if(
+                    apc_clear_cache() &&
+                    apc_clear_cache('user') &&
+                    apc_clear_cache('opcode')
+                )
+                {
+                    Mage::getSingleton('adminhtml/session')->addSuccess('APC cache flushed successfully.');
+                }
+                else
+                {
+                    Mage::getSingleton('adminhtml/session')->addError('Something went wrong while flushing APC cache.');
+                }
+                $this->_redirect('adminhtml/cache/index');
+            }
+            else
+            {
+                Mage::getSingleton('adminhtml/session')->addNotice('APC is not installed.');
+                $this->_redirect('adminhtml/cache/index');
+            }
+        }
+    }
