729a1e358cedb2b5ece8102a794a4c56b8d3a1df
Zvonimir Buric <zvonimir.buric@gmail.com>
2014-01-13 14:57:01 +0100

    adding header comment blocks

--- app/code/community/Inchoo/Apc/controllers/Adminhtml/ClearapcController.php
+++ app/code/community/Inchoo/Apc/controllers/Adminhtml/ClearapcController.php
@@ -1,29 +1,36 @@
 <?php
 
-    class Inchoo_Apc_Adminhtml_ClearapcController extends Mage_Adminhtml_Controller_Action
+/*
+ * The "clear" controller.
+ *
+ * @author  Zvonimir Buric <zvonimir.buric@gmail.com>
+ * @link    https://github.com/buric/Inchoo_Apc
+ */
+
+class Inchoo_Apc_Adminhtml_ClearapcController extends Mage_Adminhtml_Controller_Action
+{
+    public function indexAction()
     {
-        public function indexAction()
+        if(function_exists('apc_clear_cache'))
         {
-            if(function_exists('apc_clear_cache'))
+            if(
+                apc_clear_cache() &&
+                apc_clear_cache('user') &&
+                apc_clear_cache('opcode')
+            )
             {
-                if(
-                    apc_clear_cache() &&
-                    apc_clear_cache('user') &&
-                    apc_clear_cache('opcode')
-                )
-                {
-                    Mage::getSingleton('adminhtml/session')->addSuccess('APC cache flushed successfully.');
-                }
-                else
-                {
-                    Mage::getSingleton('adminhtml/session')->addError('Something went wrong while flushing APC cache.');
-                }
-                $this->_redirect('adminhtml/cache/index');
+                Mage::getSingleton('adminhtml/session')->addSuccess('APC cache flushed successfully.');
             }
             else
             {
-                Mage::getSingleton('adminhtml/session')->addNotice('APC is not installed.');
-                $this->_redirect('adminhtml/cache/index');
+                Mage::getSingleton('adminhtml/session')->addError('Something went wrong while flushing APC cache.');
             }
+            $this->_redirect('adminhtml/cache/index');
+        }
+        else
+        {
+            Mage::getSingleton('adminhtml/session')->addNotice('APC is not installed.');
+            $this->_redirect('adminhtml/cache/index');
         }
     }
+}
