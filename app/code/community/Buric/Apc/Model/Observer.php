b8788d287921dddce5f7a10622fc7c65dacfc090
Zvonimir Buric <zvonimir.buric@gmail.com>
2014-03-05 10:39:21 +0100

    version 2.0.0 - no more templates. using injectHtml method

--- app/code/community/Buric/Apc/Model/Observer.php
+++ app/code/community/Buric/Apc/Model/Observer.php
@@ -12,4 +12,33 @@ class Buric_Apc_Model_Observer {
     public function clearApc() {
         return apc_clear_cache() && apc_clear_cache('user') && apc_clear_cache('opcode');
     }
+
+    public function injectHtml(Varien_Event_Observer $observer) {
+        $block  = $observer->getBlock();
+
+        if($block instanceof Mage_Adminhtml_Block_Cache_Additional) {
+            $transport = $observer->getTransport();
+
+            $insert =
+                '<tr>
+                    <td class="scope-label">
+                        <button onclick="setLocation(\'' . Mage::helper('adminhtml')->getUrl('adminhtml/clearapc/index') . '\')" type="button" class="scalable">
+                            <span>' . Mage::helper('adminhtml')->__('Flush APC Cache') . '</span>
+                        </button>
+                    </td>
+                    <td class="scope-label">' . Mage::helper('adminhtml')->__('APC user and system cache.') . '</td>
+                </tr>';
+
+            $dom = new DOMDocument();
+
+            $dom->loadHTML($transport->getHtml());
+
+            $td = $dom->createDocumentFragment();
+            $td->appendXML($insert);
+
+            $dom->getElementsByTagName('table')->item(1)->insertBefore($td, $dom->getElementsByTagName('table')->item(1)->firstChild);
+
+            $transport->setHtml($dom->saveHTML());
+        }
+    }
 }
